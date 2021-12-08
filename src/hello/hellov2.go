355a37928743a4092383a74903c6bf246a2c6c4b
Ivo Rafael Santos Menezes <ivo@mpf.mp.br>
2021-12-08 13:16:12 -0300

    Fim da aula03

--- src/hello/hellov2.go
+++ src/hello/hellov2.go
@@ -1,24 +1,65 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
 
+	exibeIntroducao()
+
+	exibeMenu()
+
+	comando := leComando()
+	//fmt.Scanf("%d", &comando)
+	//Ou
+	//fmt.Scan(&comando) //Neste caso a função já sabe que o valor que pode ser recebido é do tipo inteiro, pois é o que foi declarado
+	//fmt.Println("O endereço da minha variável comando é:", &comando)
+	//fmt.Println("O comando escolhido foi", comando)
+
+	//if comando == 1 {
+	//	fmt.Println("Monitorando ...")
+	//} else if comando == 2 {
+	//	fmt.Println("Exibindo Logs ...")
+	//} else if comando == 0 {
+	//	fmt.Println("Saindo do programa")
+	//} else {
+	//	fmt.Println("Não conheço este comando")
+	//}
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando ...")
+	case 2:
+		fmt.Println("Exibindo Logs ...")
+	case 0:
+		fmt.Println("Saindo do programa")
+		os.Exit(0) //Informa ao SO que o programa está sendo encerrado da forma esperada.
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1) //Informa ao SO que o programa não sendo encerrado da forma inesperada - com erro.
+	}
+}
+
+func exibeIntroducao() {
 	nome := "Ivo Rafael"
 	versao := 1.1
 
 	fmt.Println("Olá Sr.", nome)
 	fmt.Println("Este programa está na versão: ", versao)
+}
 
+func exibeMenu() {
 	fmt.Println("1-Iniciar Monitoramento")
 	fmt.Println("2-Exibir Logs")
 	fmt.Println("0-Sair do Programa")
+}
 
-	var comando int
+func leComando() int {
+	var comandoLido int
 	//fmt.Scanf("%d", &comando)
 	//Ou
-	fmt.Scan(&comando) //Neste caso a função já sabe que o valor que pode ser recebido é do tipo inteiro, pois é o que foi declarado
-	fmt.Println("O endereli da minha variável comando é:" & comando)
-	fmt.Println("O comando escolhido foi", comando)
+	fmt.Scan(&comandoLido) //Neste caso a função já sabe que o valor que pode ser recebido é do tipo inteiro, pois é o que foi declarado
+	fmt.Println("O comando escolhido foi", comandoLido)
 
+	return comandoLido
 }
